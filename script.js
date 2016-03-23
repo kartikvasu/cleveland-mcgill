0f969a2ab2798e7bd398cc3b620a09122579dd19
Kartik <kartiktv97@gmail.com>
2016-03-23 16:02:21 -0400

    Added errors

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 function experiment() {
-  
+  //attributes of the experiment class
   this.list = ['insert/intro.html', 'insert/first.html', 'insert/second.html',
   'insert/third.html', 'insert/results.html', 'insert/thanks.html']; //list of insertions to be loaded
   this.counter = 0, //counter for looping the list
   this.userData = []; //array for holding all the user inputs
   this.corrects = []; //array for holding all the correct answers
+  this.percerrors = []; //array for holding all the errors
 
   var that = this;
-
+  //function definition for experiment class
   this.goNext = function() {
     console.log(this)
     console.log(that)
@@ -15,17 +16,23 @@ function experiment() {
       d3.select('#nextbtn').on('click', function() {  
       d3.html(that.list[that.counter], function(error, data) {
         if(error) console.log(error);
-        handleClick();
+        getValue();
         d3.select('#container').html('');
         d3.select('#container').node().appendChild(data);
-        ++that.counter; //increment index of the array
+
+        if(that.counter == list.length - 1) {
+          d3.select('#nextbtn').html('');
+          console.log(that.counter);
+        } else {
+          ++that.counter; //increment index of the array
+        }
         })
       });
     }
   }
 
-  function handleClick(){
-
+  function getValue(){
+  //function for storing the experiment subject's entered values
     for(var i = 0; i < list.length; i++){
       if(document.getElementById('result' + i)){
         console.log(document.getElementById('result' + i).value);
@@ -36,7 +43,6 @@ function experiment() {
   }
 
   return this;
-
 }
 
 
